9786a93f4ee5c84ec74975edce99ed69467aee43
Leeeuswa <leeco6578@naver.com>
2026-05-20 16:58:23 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/petmilyday/PetmilydayApplication.java
@@ -0,0 +1,13 @@
+package com.petmilyday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmilydayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetmilydayApplication.class, args);
+    }
+
+}
