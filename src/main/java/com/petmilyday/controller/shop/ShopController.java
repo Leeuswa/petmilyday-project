5efc9436d8c9a8df344739bdeebdb7a681f4c47c
n1mppp <parkjm4288@naver.com>
2026-05-22 21:46:23 +0900

    샵 카테고리 필터링 및 레이아웃 구현
    샵 카테고리 별 상품 필터링 기능 구현
    
    서버 샵 페이지 탭 활성화 유지
    
    물품 이름 길면 상품 UI 버튼 높낮이 안맞는 부분 정렬

--- src/main/java/com/petmilyday/controller/shop/ShopController.java
+++ src/main/java/com/petmilyday/controller/shop/ShopController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,16 +14,21 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class ShopController {
-
-
     private final ProductService productService;
-
     @GetMapping("/shop")
-    public String showShopPage(Model model) {
-        List<ProductResponseDto> products = productService.getAllProducts();
+    public String showShopPage(@RequestParam(required = false) String category, Model model) {
+        List<ProductResponseDto> products;
+
+        if (category == null || category.isEmpty() || category.equals("ALL")) {
+            products = productService.getAllProducts();
+        } else {
+            products = productService.getProductsByCategory(category);
+        }
 
         model.addAttribute("productList", products);
-    return "shop";
+        model.addAttribute("activeTab","shop");
+
+        return "shop";
     }
 
 
