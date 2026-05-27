96f73a2181f4278b7ca179b79edbdfeda0653038
Leeeuswa <leeco6578@naver.com>
2026-05-27 21:43:53 +0900

    feat: 병원 검색 및 필터 기능 구현

--- src/main/java/com/petmilyday/dto/hospital/HospitalRequestDTO.java
+++ src/main/java/com/petmilyday/dto/hospital/HospitalRequestDTO.java
@@ -1,11 +1,9 @@
 package com.petmilyday.dto.hospital;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
--- src/main/java/com/petmilyday/repository/hospital/HospitalRepository.java
+++ src/main/java/com/petmilyday/repository/hospital/HospitalRepository.java
@@ -3,7 +3,7 @@ package com.petmilyday.repository.hospital;
 import com.petmilyday.entity.hospital.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+public interface HospitalRepository extends JpaRepository<Hospital, Long> ,HospitalRepositoryCustom {
 
 
 }
