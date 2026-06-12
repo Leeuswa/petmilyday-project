1ca95b998a09261fe321ee6b051d8737a6e6cf2a
endy <ldg7711@gmail.com>
2026-06-12 17:31:44 +0900

    모임 게시판 구현

---
+++ src/main/java/com/petmilyday/service/community/MeetupService.java
@@ -0,0 +1,22 @@
+package com.petmilyday.service.community;
+
+import com.petmilyday.dto.community.MeetupPostDTO;
+import java.util.List;
+
+public interface MeetupService {
+
+    Long registerMeetup(String username, MeetupPostDTO dto);
+
+    List<MeetupPostDTO> getList();
+
+    // 모임 상세 조회
+    MeetupPostDTO read(Long id, String username);
+
+    // 모임 참여
+    void joinMeetup(Long id, String username);
+
+    // 모임 참여 취소
+    void cancelMeetup(Long id, String username);
+
+    void deleteMeetup(Long id, String username);
+}
