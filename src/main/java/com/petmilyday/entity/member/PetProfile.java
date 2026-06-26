076359e92a12f26dabb94f19adaefda1cc05c756
endy7540 <ldg7711@gmail.com>
2026-06-26 21:51:40 +0900

    로그인 마무리

--- src/main/java/com/petmilyday/entity/member/PetProfile.java
+++ src/main/java/com/petmilyday/entity/member/PetProfile.java
@@ -1,10 +1,14 @@
 package com.petmilyday.entity.member;
 
+import com.petmilyday.entity.reservation.Reservation;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -39,4 +43,7 @@ public class PetProfile {
 
     @Column(length = 500)
     private String photoUrl;
+
+    @OneToMany(mappedBy = "pet", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Reservation> reservations = new ArrayList<>();
 }
